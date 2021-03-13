f4c2c3a7516bef4f58d0a621a1bf99af0a4012e0
be beet <63141491+beetcb@users.noreply.github.com>
2021-03-13 22:35:58 +0800

    feat: init repo

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite ( play with it )" />
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style>
+/* ./src/index.css */
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>

4c1d93bcc99da5e0f3951b683c2e447a4011b80c
be beet <63141491+beetcb@users.noreply.github.com>
2021-03-14 21:44:51 +0800

    feat: deep dive to vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,19 +6,14 @@
       class="block m-2 mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       @click="state.count++"
     >
-      plus 1: {{ state.count }}
-    </button>
-    <button
-      class="block m-2 mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      @click="state.count--"
-    >
-      minus 1: {{ state.count }}
+      plus: {{ state.count }}
     </button>
   </div>
 </template>
 
 <script setup>
 import { reactive } from 'vue'
+// import { parse } from '@iarna/toml'
 
-const state = reactive({ count: 0 })
+const state = reactive({ count: 0, input: '' })
 </script>
